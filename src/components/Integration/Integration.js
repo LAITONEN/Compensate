379deffeeddf7318cb8b5b9182c67d065cfe6173
Eduard Rastoropov <eduard@rastoropov@gmail.com>
2020-02-27 16:27:55 +0200

    Implemented responsive Integration page

--- src/components/Integration/Integration.js
+++ src/components/Integration/Integration.js
@@ -4,12 +4,14 @@ import styles from './Integration.module.scss'
 
 const Integration = (props) => {
   return (
-    <div className={styles.wrapper}>
-      <div>
-        <div>Compensate can be integrated into all kinds of products and services.</div>
-        <div>With a recognizable trademark, we are creating a movement that scales globally.</div>
+    <div className={styles.mainWrapper}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.text}>
+          <div className={styles.title}>Compensate can be integrated into all kinds of products and services.</div>
+          <div className={styles.subtitle}>With a recognizable trademark, we are creating a movement that scales globally.</div>
+        </div>
+        <div className={styles.hotelAppImage} />
       </div>
-      <img src={'../../assets/hotel-mockup.png'} alt="" />
     </div>
   );
 }
