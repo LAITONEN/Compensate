bedd03423528927891140cac84c5035bf4838614
Eduard Rastoropov <eduard@rastoropov@gmail.com>
2020-03-01 20:50:22 +0200

    Made Impact page responsive.

--- src/components/Impact/Impact.js
+++ src/components/Impact/Impact.js
@@ -3,25 +3,33 @@ import styles from './Impact.module.scss'
 
 const Impact = (props) => {
   return (
-    <div>
-      <div>Impact</div>
-      <div>In 9 months here is what we've achieved</div>
-      <div>
-        <div>
-          <div>63K</div>
-          <div>individuals have measured their carbon footprint</div>
+    <div className={styles.mainWrapper}>
+      <div className={styles.title}>Impact</div>
+      <div className={styles.subtitle}>In 9 months here is what we've achieved</div>
+      <div className={styles.metricsWrapper}>
+        <div className={styles.metricSquare}>
+          <div className={styles.metricWrapper}>
+            <div className={styles.data}>63K</div>
+            <div className={styles.domain}>individuals have measured their carbon footprint</div>
+          </div>
         </div>
-        <div>
-          <div>513 540 €</div>
-          <div>in compensation payments from subscriptions and through business partners towards carbon capture</div>
+        <div className={styles.metricSquare}>
+          <div className={styles.metricWrapper}>
+            <div className={styles.data}>513 540 €</div>
+            <div className={styles.domain}>in compensation payments from subscriptions and through business partners towards carbon capture</div>
+          </div>
         </div>
-        <div>
-          <div>1 512 225</div>
-          <div>trees planted</div>
+        <div className={styles.metricSquare}>
+          <div className={styles.metricWrapper}>
+            <div className={styles.data}>1 512 225</div>
+            <div className={styles.domain}>trees planted</div>
+          </div>
         </div>
-        <div>
-          <div>870</div>
-          <div>football fields of forest preserved</div>
+        <div className={styles.metricSquare}>
+          <div className={styles.metricWrapper}>
+            <div className={styles.data}>870</div>
+            <div className={styles.domain}>football fields of forest preserved</div>
+          </div>
         </div>
       </div>
     </div>
