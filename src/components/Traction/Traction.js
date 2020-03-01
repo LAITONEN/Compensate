1c43b18252e198e26a6aa1325a3c06809e744d3b
Eduard Rastoropov <eduard@rastoropov@gmail.com>
2020-03-01 20:50:13 +0200

    Made Traction page responsive.

--- src/components/Traction/Traction.js
+++ src/components/Traction/Traction.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import styles from './Traction.module.scss'
+import instagramImage from '../../assets/instagram-mockup.png'
 
 const Traction = (props) => {
   return (
-    <div>
-      <img src={'../../assets/instagram-mockup.png'} alt="" />
-      <div>
-        <div>Gaining traction</div>
-        <div>
-          <div>
-            <div>13K</div>
-            <div>followers across all social media platforms</div>
-          </div>
-          <div>
-            <div>53%</div>
-            <div>open rate for our newsletter</div>
-          </div>
-          <div>
-            <div>1.7M</div>
-            <div>reach on Twitter</div>
-          </div>
-          <div>
-            <div>119K</div>
-            <div>video views</div>
+    <div className={styles.mainWrapper}>
+      <div className={styles.content}>
+        <img className={styles.instagramImage} src={instagramImage} alt="" />
+        <div className={styles.metricsWrapper}>
+          <div className={styles.title}>Gaining traction</div>
+          <div className={styles.metrics}>
+            <div className={styles.metricCircle}>
+              <div className={styles.data}>13K</div>
+              <div className={styles.domain}>followers across all social media platforms</div>
+            </div>
+            <div className={styles.metricCircle}>
+              <div className={styles.data}>53%</div>
+              <div className={styles.domain}>open rate for our newsletter</div>
+            </div>
+            <div className={styles.metricCircle}>
+              <div className={styles.data}>1.7M</div>
+              <div className={styles.domain}>reach on Twitter</div>
+            </div>
+            <div className={styles.metricCircle}>
+              <div className={styles.data}>119K</div>
+              <div className={styles.domain}>video views</div>
+            </div>
           </div>
         </div>
       </div>
+      <img className={styles.instagramImageMobile} src={instagramImage} alt="" />
     </div>
   );
 }
